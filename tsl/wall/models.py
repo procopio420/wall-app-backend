54c81ad66047105817a2a3c2120e0e9e047d2062
procopio420 <lucas-procopio@outlook.com>
2021-06-23 20:47:49 -0300

    feat: models

--- tsl/wall/models.py
+++ tsl/wall/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=True, default='')
+    body = models.TextField(blank=True, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
