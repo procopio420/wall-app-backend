6c888a67cdeb288841d3da59e7e3830488135f34
procopio420 <lucas-procopio@outlook.com>
2021-06-23 20:39:40 -0300

    feat: views

--- tsl/auth/views.py
+++ tsl/auth/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RegisterSerializer, MyTokenObtainPairSerializer
 
-# Create your views here.
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
